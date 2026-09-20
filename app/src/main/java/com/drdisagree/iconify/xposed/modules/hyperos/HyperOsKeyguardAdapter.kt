f2e25af133bbf1b42c7108698adaaae840133bfa
Anh Tran <162714915+AnhTran1006@users.noreply.github.com>
2026-09-21 00:00:07 +0700

    feat(hyperos4): hook verified keyguard blueprint targets

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/hyperos/HyperOsKeyguardAdapter.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/hyperos/HyperOsKeyguardAdapter.kt
@@ -21,13 +21,14 @@ class HyperOsKeyguardAdapter(context: Context) : ModPack(context) {
     override fun handleLoadPackage(param: LoadPackageParam) {
         if (!HyperOsEnvironment.usesSystemUiBackend(param.packageName)) return
         hookRootView()
-        hookRootBinder()
+        hookBlueprint()
         hookClockProvider()
     }
 
     private fun hookRootView() {
         val cls = findClass(
             "com.android.keyguard.widget.HyperOSKeyguardRootView",
+            "com.android.systemui.keyguard.ui.view.KeyguardRootView",
             suppressError = true
         ) ?: return
 
@@ -40,37 +41,44 @@ class HyperOsKeyguardAdapter(context: Context) : ModPack(context) {
         }
     }
 
-    private fun hookRootBinder() {
+    private fun hookBlueprint() {
         val cls = findClass(
-            "com.android.systemui.keyguard.ui.binder.KeyguardRootViewBinder",
+            "com.android.keyguard.blueprint.HyperOsKeyguardBlueprint",
+            "com.android.keyguard.blueprint.KeyguardPanelViewSection",
             "com.android.keyguard.ui.binder.KeyguardPanelViewBinder",
             suppressError = true
         ) ?: return
 
         cls.hookMethod(
             "bind",
+            "start",
             "onViewAttached",
             "update"
         ).suppressError().runAfter { hookParam ->
-            if (hideLockIcon) hideKnownLockIcon(hookParam.thisObject)
+            if (hideLockIcon) {
+                (hookParam.thisObject as? View)?.let(::hideKnownLockIcon)
+                hookParam.args.filterIsInstance<View>().forEach(::hideKnownLockIcon)
+            }
         }
     }
 
     private fun hookClockProvider() {
         val cls = findClass(
             "com.miui.keyguard.clock.HyperOSDefaultClockProvider",
+            "com.android.keyguard.clock.MiuiKeyguardSingleClock",
+            "com.android.keyguard.clock.MiuiKeyguardDualClock",
             suppressError = true
         ) ?: return
 
         cls.hookMethod(
             "getClockView",
             "createClockView",
-            "provideClock"
+            "provideClock",
+            "onFinishInflate"
         ).suppressError().runAfter { }
     }
 
-    private fun hideKnownLockIcon(target: Any) {
-        val view = target as? View ?: return
+    private fun hideKnownLockIcon(view: View) {
         for (name in listOf("lock_icon", "keyguard_lock_icon", "lock_icon_view")) {
             val id = view.resources.getIdentifier(name, "id", view.context.packageName)
             if (id != 0) view.findViewById<View>(id)?.visibility = View.GONE
