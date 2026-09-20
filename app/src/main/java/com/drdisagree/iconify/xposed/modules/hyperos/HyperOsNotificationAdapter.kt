37b257d15c8133c716661c215db9a7e2fb57e8e2
Anh Tran <162714915+AnhTran1006@users.noreply.github.com>
2026-09-21 00:00:33 +0700

    feat(hyperos4): add notification icon container adapter

---
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/hyperos/HyperOsNotificationAdapter.kt
@@ -0,0 +1,56 @@
+package com.drdisagree.iconify.xposed.modules.hyperos
+
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import com.drdisagree.iconify.data.keys.XposedKey
+import com.drdisagree.iconify.hyperos.HyperOsEnvironment
+import com.drdisagree.iconify.xposed.ModPack
+import com.drdisagree.iconify.xposed.modules.extras.utils.toolkit.XposedHook.Companion.findClass
+import com.drdisagree.iconify.xposed.modules.extras.utils.toolkit.hookMethod
+import com.drdisagree.iconify.xposed.utils.XPrefs.Xprefs
+import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
+import kotlin.math.roundToInt
+
+class HyperOsNotificationAdapter(context: Context) : ModPack(context) {
+
+    private var iconLimit = -1
+
+    override fun updatePrefs(vararg key: String) {
+        iconLimit = Xprefs.getFloat(XposedKey.NOTIFICATION_ICONS_LIMIT).roundToInt()
+    }
+
+    override fun handleLoadPackage(param: LoadPackageParam) {
+        if (!HyperOsEnvironment.usesSystemUiBackend(param.packageName)) return
+        if (iconLimit < 0) return
+
+        val cls = findClass(
+            "com.android.systemui.statusbar.notification.icon.NotificationIconContainer",
+            "com.android.systemui.statusbar.notification.icon.ui.viewbinder.NotificationIconContainerViewBinder",
+            suppressError = true
+        ) ?: return
+
+        cls.hookMethod(
+            "onLayout",
+            "updateState",
+            "bind"
+        ).suppressError().runAfter { hookParam ->
+            val root = hookParam.thisObject as? ViewGroup ?: return@runAfter
+            applyLimit(root)
+        }
+    }
+
+    private fun applyLimit(root: ViewGroup) {
+        var visible = 0
+        for (i in 0 until root.childCount) {
+            val child = root.getChildAt(i)
+            if (child.visibility == View.GONE) continue
+
+            if (visible++ < iconLimit) {
+                child.visibility = View.VISIBLE
+            } else {
+                child.visibility = View.GONE
+            }
+        }
+    }
+}
