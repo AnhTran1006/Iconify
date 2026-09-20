d6c73247a9eb9d30186ffd4b7220400ecde57f84
Anh Tran <162714915+AnhTran1006@users.noreply.github.com>
2026-09-20 22:59:08 +0700

    HyperOS: add capability model

---
+++ app/src/main/java/com/drdisagree/iconify/hyperos/HyperOsCapabilities.kt
@@ -0,0 +1,10 @@
+package com.drdisagree.iconify.hyperos
+
+data class HyperOsCapabilities(
+    val wifiIcons: Boolean = false,
+    val mobileIcons: Boolean = false,
+    val signalIcons: Boolean = false,
+    val batteryIcons: Boolean = false,
+    val chargingIcons: Boolean = false,
+    val statusBarLayout: Boolean = false,
+)
