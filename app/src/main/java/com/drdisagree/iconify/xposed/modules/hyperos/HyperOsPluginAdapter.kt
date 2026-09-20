0a49c153439d218f071c4e498a7d7aac28de158b
Anh Tran <162714915+AnhTran1006@users.noreply.github.com>
2026-09-20 23:54:27 +0700

    feat(hyperos4): bridge Control Center blur preferences to plugin resources

--- app/src/main/java/com/drdisagree/iconify/xposed/modules/hyperos/HyperOsPluginAdapter.kt
+++ app/src/main/java/com/drdisagree/iconify/xposed/modules/hyperos/HyperOsPluginAdapter.kt
@@ -1,11 +1,14 @@
 package com.drdisagree.iconify.xposed.modules.hyperos
 
 import android.content.Context
+import com.drdisagree.iconify.data.keys.XposedKey
 import com.drdisagree.iconify.hyperos.HyperOsEnvironment
 import com.drdisagree.iconify.xposed.ModPack
+import com.drdisagree.iconify.xposed.modules.extras.utils.toolkit.ResourceHookManager
 import com.drdisagree.iconify.xposed.modules.extras.utils.toolkit.XposedHook.Companion.findClass
 import com.drdisagree.iconify.xposed.modules.extras.utils.toolkit.hookMethod
 import com.drdisagree.iconify.xposed.modules.extras.utils.toolkit.log
+import com.drdisagree.iconify.xposed.utils.XPrefs.Xprefs
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 
 /**
@@ -17,7 +20,15 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
  */
 class HyperOsPluginAdapter(context: Context) : ModPack(context) {
 
-    override fun updatePrefs(vararg key: String) = Unit
+    private var blurEnabled = false
+    private var blurRadius = 23
+
+    override fun updatePrefs(vararg key: String) {
+        Xprefs.apply {
+            blurEnabled = getBoolean(XposedKey.QUICK_SETTINGS_BLUR)
+            blurRadius = getInt(XposedKey.QUICK_SETTINGS_BLUR_RADIUS)
+        }
+    }
 
     override fun handleLoadPackage(param: LoadPackageParam) {
         if (!HyperOsEnvironment.usesPluginBackend(param.packageName)) return
@@ -26,14 +37,8 @@ class HyperOsPluginAdapter(context: Context) : ModPack(context) {
         hookControlCenter()
         hookBrightness()
         hookDynamicIsland()
-
-        val lifecycle = findClass(
-            "miui.systemui.plugins.domain.interactor.PluginLifecycleInteractor",
-            suppressError = true
-        )
-        if (lifecycle != null) {
-            log(this, "HyperOS 4 PluginLifecycleInteractor detected")
-        }
+        hookPluginLifecycle()
+        bridgeBlurResource()
     }
 
     private fun hookControlCenter() {
@@ -84,5 +89,32 @@ class HyperOsPluginAdapter(context: Context) : ModPack(context) {
         ).suppressError().runAfter {
             log(this, "HyperOS 4 Dynamic Island target resolved")
         }
+    }
+
+    private fun hookPluginLifecycle() {
+        val lifecycle = findClass(
+            "miui.systemui.plugins.domain.interactor.PluginLifecycleInteractor",
+            suppressError = true
+        ) ?: return
+
+        lifecycle.hookMethod(
+            "start",
+            "stop",
+            "onStart",
+            "onStop"
+        ).suppressError().runAfter {
+            log(this, "HyperOS 4 plugin lifecycle target resolved")
+        }
+    }
+
+    private fun bridgeBlurResource() {
+        if (!blurEnabled) return
+
+        ResourceHookManager
+            .hookDimen()
+            .whenCondition { true }
+            .forPackageName(HyperOsEnvironment.SYSTEMUI_PLUGIN_PACKAGE)
+            .addResource("max_window_blur_radius") { blurRadius }
+            .apply()
     }
 }
